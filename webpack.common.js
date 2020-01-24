7914338543a59f776a52cb5e8fc68a9ac3151671
kdord <katya.koliada49@gmail.com>
2020-01-24 12:26:08 +0200

    extract amd minify css

--- webpack.common.js
+++ webpack.common.js
@@ -3,21 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/index.js', vendor: './src/vendor.js' },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/template.html'
-    })
-  ],
+
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', // 3. injects styles into dom
-          'css-loader', // 2. turns css onto commonjs
-          'sass-loader' // 1. turnd sass into css
-        ]
-      },
       {
         test: /\.html$/,
         use: ['html-loader']
