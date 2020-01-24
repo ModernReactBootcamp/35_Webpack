a43a8811b2f976ae2258a1ac5c22076d3219ee88
kdord <katya.koliada49@gmail.com>
2020-01-24 09:58:04 +0200

    add sass loader, override bootstrap color

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.scss$/,
+        use: [
+          'style-loader', // 3. injects styles into dom
+          'css-loader', // 2. turns css onto commonjs
+          'sass-loader' // 1. turnd sass into css
+        ]
       }
     ]
   }
